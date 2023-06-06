949bf05ab28632799932a83969e53ce3e0fce813
BrunoGomes25 <zeinitrudesti20@gmail.com>
2023-06-05 21:56:33 -0300

    exercicio de funçao e objeto

--- aulas/JS/Exercicios/obj,function e array/objfun.js
+++ aulas/JS/Exercicios/obj,function e array/objfun.js
@@ -1,2 +1,23 @@
-//exercicio de hj porem so vou fazer amanha
-// procastinando 2, triste d+
+function meuScopo (){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
+    const amigoes = [];
+    function recebeEventoForm (evento){
+        evento.preventDefault()
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        console.log(nome, sobrenome, peso, altura);
+        amigoes.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+        console.log(amigoes);
+        resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>`
+    }
+    form.addEventListener('submit', recebeEventoForm)
+}
+meuScopo();
