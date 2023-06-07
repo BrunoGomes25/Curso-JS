443847dfed7989932ccedd931eadd9dcb03b21bf
BrunoGomes25 <zeinitrudesti20@gmail.com>
2023-06-07 18:42:36 -0300

    terminando a aula de ontem

--- aulas/JS/Operadores Logicos/OpLogicos.js
+++ aulas/JS/Operadores Logicos/OpLogicos.js
@@ -2,9 +2,14 @@
 Operadores logicos
 && -> and -> E todas as condiçoes precisam ser verdadeiras
 
-|| -> or -> ou
+|| -> or -> ou pelo menos uma expressao tem q ser verdadeira
 
-! -> not -> nao
+! -> not -> nao    inverte o valor
 */
+console.log(!false)
 
-console.log(true && true && true)
+//const expressaoOR = true||false
+//console.log(expressaoOR)
+
+//const expressaoAnd = true && true && true
+//console.log(expressaoAnd)
