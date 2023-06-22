cc2198bf04adf37bf0a5bae0d6fd764a0b32b3b6
BrunoGomes25 <zeinitrudesti20@gmail.com>
2023-06-21 22:36:50 -0300

    date, if e ternario

---
+++ aulas/JS/operador ternario/OPternario.js
@@ -0,0 +1,4 @@
+// ?: operaçao ternaria é usada com "?" ponto de interrogaçao
+const pontuacaoUSuario = 1000
+const nivelUsuario = pontuacaoUSuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+console.log(nivelUsuario) 
