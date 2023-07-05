e14cce0e889bf145eefaa8bd7846237568211849
BrunoGomes25 <zeinitrudesti20@gmail.com>
2023-07-04 23:17:55 -0300

    nodelist

---
+++ aulas/JS/IF-Else/ex/if.js
@@ -0,0 +1,13 @@
+const assuntoConversa = prompt ("qual o assunto de hj chapa?")
+
+if (assuntoConversa === "ufc") {
+  console.log("UFC sabado chapa?");
+} else if (assuntoConversa === "Academia") {
+  console.log("Jiu-jitsu amanha chapa?");
+} else if (assuntoConversa === "fofoca") {
+  console.log("falar sobre a vida alheia sabado chapa?");
+} else if (assuntoConversa === "mulher") {
+  console.log("Va tomar no seu cu");
+} else {
+  console.log("tem else nao chapa");
+}
