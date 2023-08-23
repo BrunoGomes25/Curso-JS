0087365cf6d85cf330522cdb6abb4ff3b9e692b5
BrunoGomes25 <zeinitrudesti20@gmail.com>
2023-08-22 21:17:19 -0300

    Prototype

--- aulas/JS/Prototype/prototype.js
+++ aulas/JS/Prototype/prototype.js
@@ -1,1 +1,13 @@
-"nada mudou"
+// construtora -> molde (classe)
+function Pessoa(nome, sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+}
+
+// instancia
+
+const pessoa1 = new Pessoa ('Luiz', 'O.'); // <- Pessoa = funcao construtora
+const pessoa2 = new Pessoa ('Maria', 'A.'); // <- Pessoa = funcao construtora
+console.dir(pessoa1);
+console.dir(pessoa2);
